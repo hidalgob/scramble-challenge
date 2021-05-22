0b6e0acded4ac8ee2747fea1dbc69d810867bd05
Bruno Hidalgo <bruno.hidalgo@inallmedia.com>
2021-05-22 18:11:01 -0300

    add: basic scramble tests

---
+++ test/clj/scramble_test.clj
@@ -0,0 +1,12 @@
+(ns scramble-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [scramble :as scramble]))
+
+
+(deftest scramble-tests
+  (testing "scramble works on given tests"
+    (is (true? (scramble/scramble? "rekqodlw" "world")))
+    (is (true? (scramble/scramble? "cedewaraaossoqqyt" "codewars")))
+    (is (false? (scramble/scramble? "katas" "steak")))))
+
+
