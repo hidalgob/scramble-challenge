a9c22411a7db98bd1cfbd1b38133f8664afd4b35
Bruno Hidalgo <bruno.hidalgo@inallmedia.com>
2021-05-23 20:39:07 -0300

    add: improved html and validation

--- src/cljs/scramble_challenge/view/scramble.cljs
+++ src/cljs/scramble_challenge/view/scramble.cljs
@@ -2,9 +2,12 @@
   (:require [re-frame.core :refer [dispatch subscribe]]
             [reagent.core :as r]))
 
+(defn only-alpha? [str]
+  (boolean (re-matches #"^[a-z\-]+$" str)))
+
 (defn disabled-button? [pool sample]
-  (or (empty? pool)
-      (empty? sample)))
+  (or (not (only-alpha? pool))
+      (not (only-alpha? sample))))
 
 (defn scramble-view []
   (let [pool     (r/atom "")
@@ -14,27 +17,34 @@
       [:section {:class "section"}
        [:div {:class "container"}
         [:div {:class "content"}
-         [:h1 "Scramble!"]
+         [:h1 "Scramble challenge"]
+         [:p "Returns true if a portion of Pool characters can be rearranged to match Sample, otherwise returns false"]
+         [:p [:b "Only accepts lower-case letters. [a-z]"]]
+         [:p "Example:"]
+         [:p "Pool: \"rekqodlw\""]
+         [:p "Sample: \"world\""]
+         [:p "Result: True"]
          [:div {:class "inputs"}
           [:div
            [:label "Pool: "]
            [:input {:name        "pool"
-                    :placeholder "Input pool"
                     :type        :text
                     :value       @pool
+                    :pattern     "[a-z]+"
                     :on-change   #(reset! pool (-> % .-target .-value))}]]
           [:div
            [:label "Sample: "]
            [:input {:name        "sample"
-                    :placeholder "Input sample"
                     :type        :text
                     :value       @sample
+                    :pattern     "[a-z]+"
                     :on-change   #(reset! sample (-> % .-target .-value))}]]
           [:button {:type     :submit
                     :on-click #(dispatch [:scramble/run @pool @sample])
                     :disabled (disabled-button? @pool @sample)}
            "Check Scramble"]]
+         (only-alpha? @pool)
          (when (not-empty @scramble)
            [:div {:class "result"}
             [:div
-             [:label "Result: " (:result @scramble)]]])]]])))
+             [:label "Result: " (str (:result @scramble))]]])]]])))
